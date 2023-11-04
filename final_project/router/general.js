5523be9308c2f8acbc0a44e9f5bf23a4f4441311
Your Name <you@example.com>
2023-11-04 10:53:17 -0400

    mmm

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,27 +24,53 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const bookList = Object.values(books);
+  return res.status(200).send(JSON.stringify({bookList}, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const isbn = req.params.isbn;
+  if(books[isbn]) {
+    return res.status(200).json(books[isbn]);
+  } else {
+    return res.status(404).json({message: "Book not found."});
+  }
+  
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let booksbyauthor = [];
+  let isbns = Object.keys(books);
+  isbns.forEach((isbn) => {
+    if(books[isbn]["author"] === req.params.author) {
+          booksbyauthor.push({"isbn":isbn,
+                              "title":books[isbn]["title"],
+                              "reviews":books[isbn]["reviews"]});
+      }
+  });
+
+  return res.status(200).send(JSON.stringify({booksbyauthor}, null, 4))
 });
 
+
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+    let booksbytitle = []; // Use consistent variable names
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+        if (books[isbn]["title"] === req.params.title) {
+            booksbytitle.push({
+                "isbn": isbn,
+                "title": books[isbn]["title"],
+                "reviews": books[isbn]["reviews"]
+            });
+        }
+    });
+    return res.status(200).send(JSON.stringify({ booksbytitle }, null, 4));
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
